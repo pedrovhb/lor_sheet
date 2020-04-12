8730cd5aa74117a201b3a402aa431488eebdb041
Pedro Batista <pedrovhb@gmail.com>
2020-04-12 20:35:32 -0300

    Correct typo.

--- lor_sheet/src/components/CardPicker.svelte
+++ lor_sheet/src/components/CardPicker.svelte
@@ -5,7 +5,7 @@
 <div class="w-3/4 m-auto flex flex-col justify-center p-3 shadow my-6">
     <div class="mx-4 text-center">
         <p class="my-2">Click the filters above to select your opponent's region and to toggle showing only quick+burst spells.</p>
-        <p class="my-2">Use the text field on below to enable or disable cards from being shown.</p>
+        <p class="my-2">Use the text field below to enable or disable cards from being shown.</p>
         <p class="my-2">Cards will only be enabled if their exact name is matched, so you can add a "#" to exclude them.</p>
     </div>
     <textarea bind:value={$enabledCards} class="mx-4 my-6" cols="50" rows="6"></textarea>
